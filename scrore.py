cf843c07d69815bb42d1eb3a6c6e7f43d76e5f73
Mausham Bista <maushambista0@gmail.com>
2023-10-26 19:06:24 -0600

    added file so it can store high score

--- scrore.py
+++ scrore.py
@@ -3,8 +3,9 @@ from turtle import Turtle
 class Score(Turtle):
     def __init__(self):
         super().__init__()
+        with open("high_score.txt") as r_file:
+            self.high_score = r_file.read()
         self.scoreboard = 0
-        self.high_score = 0
         self.penup()
         self.goto(0, 320)
         self.hideturtle()
@@ -15,10 +16,12 @@ class Score(Turtle):
         self.write(f"Scrore: {self.scoreboard} High score: {self.high_score}" , move=False, align="center", font=("Courier", 20, "normal"))
 
     def game_reset(self):
-        if self.scoreboard > self.high_score:
+        if self.scoreboard > int(self.high_score):
             self.high_score = self.scoreboard
         self.scoreboard = 0
         self.update()
+        with open("high_score.txt", "w") as file:
+            file.write(str(self.high_score))
 
     # def game_over(self):
     #     self.goto(0,0)
